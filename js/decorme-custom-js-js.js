d920b01f52a5bfabc5d3665d8cba287e0ab89ea9
rimle-1 <elmirmelzetdinov@yandex.ru>
2024-09-26 00:15:43 +0300

    Доработка слайдера

--- js/decorme-custom-js-js.js
+++ js/decorme-custom-js-js.js
@@ -90,7 +90,8 @@
             $(".catalog-slider").owlCarousel({
                 loop:true,
                 margin:10,
-                nav:true,
+                nav: true,
+                navText: ['<i class="fa-solid fa-chevron-left"></i>', '<i class="fa-solid fa-chevron-right"></i>'],
                 responsive:{
                     0:{
                         items:1
