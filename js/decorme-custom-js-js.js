1b8547a90af0fb1fefcba30cc9dce3b77923e86b
rimle-1 <elmirmelzetdinov@yandex.ru>
2024-09-27 00:05:56 +0300

    Добавил карточки в каталог

--- js/decorme-custom-js-js.js
+++ js/decorme-custom-js-js.js
@@ -109,6 +109,35 @@
             });
           });
 
+        // Сounter Number
+
+        //each, prop, animate, math, text
+
+        //.animate( properties [, duration ] [, easing ] [, callback ] )
+        //"swing" - moves slower at the beginning/end, but faster in the middle
+        //"linear" - moves in a constant speed
+        //step : A function to be called after each step of the animation.  step takes: now and fx.
+        //$(selector).prop(name,value)
+
+        $('.count').each(function() {
+
+            $(this).prop('counter', 0).animate({
+        
+            counter: $(this).text()
+        
+            }, {
+        
+            duration: 3000,
+        
+            easing: 'swing',
+        
+            step: function(now) {
+        
+                $(this).text(Math.ceil(now));
+            }
+            });
+        });
+
         // Social Effects
         var ping = new Audio("");
         var timelines = {};
